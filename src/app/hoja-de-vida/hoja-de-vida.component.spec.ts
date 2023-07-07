d2ff5553b67f241c4c67eb2c1a7bedb50bc87718
jhonatanlop1 <118953731+jhonatanlop1@users.noreply.github.com>
2023-07-07 17:36:44 -0500

    Update hoja-de-vida.component.spec.ts

--- src/app/hoja-de-vida/hoja-de-vida.component.spec.ts
+++ src/app/hoja-de-vida/hoja-de-vida.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HojaDeVidaComponent } from './hoja-de-vida.component';
+
 
 describe('HojaDeVidaComponent', () => {
   let component: HojaDeVidaComponent;
